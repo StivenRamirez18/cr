eb71ea2a74ef553eb901cf7e960a8e8af61241f7
Stiven Ramirez <rstiven570@gmail.com>
2026-05-19 18:44:44 -0500

    Update API_URL to API Gateway HTTPS 2

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000'; // Cambiado temporalmente a local
-export const API_URL = 'http://restaurantreviews-env.eba-ed4sp9vv.us-east-1.elasticbeanstalk.com/';
+export const API_URL = 'https://d4d3f8uoz419y.cloudfront.net/';
