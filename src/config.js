30f8ab5d6a9a744641e1cfcf2e3d2d0ac896b51a
Stiven Ramirez <rstiven570@gmail.com>
2026-05-19 18:47:34 -0500

    Update API_URL to API Gateway HTTPS 2

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000'; // Cambiado temporalmente a local
-export const API_URL = 'https://d4d3f8uoz419y.cloudfront.net/';
+export const API_URL = 'https://d4d3f8uoz419y.cloudfront.net';
