a5c202a5825c1880181adba64f68572b269c23c2
Stiven Ramirez <rstiven570@gmail.com>
2026-05-19 20:34:54 -0500

    Update API_URL to API Gateway HTTPS 2

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = 'http://restaurantreviews-env.eba-ed4sp9vv.us-east-1.elasticbeanstalk.com';
+export const API_URL = 'https://restaurantreviews-env.eba-ed4sp9vv.us-east-1.elasticbeanstalk.com';
